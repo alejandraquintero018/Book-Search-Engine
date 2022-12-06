e7711be5a4e00c6d3cdcc848f116d37e7b9017ae
alejandraquintero018 <alejandraquintero018@gmail.com>
2022-12-05 18:10:42 -0800

    added functionality to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,33 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({ 
+  link: authLink
+})
+
 function App() {
   return (
     <Router>
